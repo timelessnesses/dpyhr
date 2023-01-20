f3ec02c7b4d6b095fc45dab6329598f687149a1c
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2023-01-20 10:20:57 +0700

    wtf

--- dpyhr/utils.py
+++ dpyhr/utils.py
@@ -9,4 +9,4 @@ def is_bot(bot: object) -> bool:
     return isinstance(bot，commands.Bot)
 
 def runner(property: typing.Union[typing.Callable, typing.Coroutine], *args, **kwargs) -> typing.Optional[typing.Any]:
-    return property() if not is_coro(property) else asyncio.run(property(*args, **kwargs))
+    return property() if not is_coro(property) else asyncio.run(property(*args,**kwargs))
